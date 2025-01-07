d870c4e7cf5375e0a7c2732b8d1f1d693335914b
cafephilia <cafephilia11@gmail.com>
2025-01-07 15:25:03 +0900

    initial commit

---
+++ src/main/java/com/cantata/tradetalent/TradetalentApplication.java
@@ -0,0 +1,13 @@
+package com.cantata.tradetalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradetalentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradetalentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cantata/tradetalent/TradetalentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantata.tradetalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradetalentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
