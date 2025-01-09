b3c9fb457e4082227acdf374cb662138951f0871
samchi <seyoung0314@gmail.com>
2025-01-09 19:12:19 +0900

    [B/add] Added methods to UserRepository

--- src/main/java/com/cantata/tradetalent/repository/UserRepository.java
+++ src/main/java/com/cantata/tradetalent/repository/UserRepository.java
@@ -12,4 +12,13 @@ public interface UserRepository {
     //중복체크용 조회 메서드
     Optional<User> findByEmail(String email);
     Optional<User> findByName(String name);
+
+    // 회원정보 조회
+    User findUser(String name);
+
+    // 회원정보 수정
+    User updateUserInfo();
+
+    // 회원정보 삭제
+    void deleteUser(User user);
 }
