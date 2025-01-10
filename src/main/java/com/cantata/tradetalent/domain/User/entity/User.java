301d6e9183d7adf70175afca66db2dc754a8297e
samchi <seyoung0314@gmail.com>
2025-01-10 14:30:14 +0900

    [B/ref] add column users table and add class filed

--- src/main/java/com/cantata/tradetalent/domain/User/entity/User.java
+++ src/main/java/com/cantata/tradetalent/domain/User/entity/User.java
@@ -11,6 +11,8 @@ public class User {
     private String name;  // 사용자 이름
     private String email;  // 이메일, 고유값
     private String profileImageUrl;  // 프로필 이미지 URL
+    private String role;
+    private String refreshToken;
     private LocalDateTime registrationDate;  // 회원가입일
     private LocalDateTime updatedAt;  // 최종 수정일
     private LocalDateTime deactivationDate;  // 회원탈퇴일
