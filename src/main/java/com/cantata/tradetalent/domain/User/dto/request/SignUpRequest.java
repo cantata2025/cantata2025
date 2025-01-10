25fbc762f08acea76ddd6d36ac4c9586e79fd88a
samchi <seyoung0314@gmail.com>
2025-01-10 14:34:33 +0900

    [B/feat] Add validation annotations SignUpRequest

--- src/main/java/com/cantata/tradetalent/domain/User/dto/request/SignUpRequest.java
+++ src/main/java/com/cantata/tradetalent/domain/User/dto/request/SignUpRequest.java
@@ -1,6 +1,9 @@
 package com.cantata.tradetalent.domain.User.dto.request;
 
 import com.cantata.tradetalent.domain.User.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter @Setter
@@ -11,8 +14,16 @@ import lombok.*;
 @Builder
 public class SignUpRequest {
 
+    @Email
+    @NotBlank (message = "이메일을 입력해주세요.")
     private String email;   // 이메일 (id) 고유값
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "비밀번호는 8자 이상, 영문과 숫자 조합이어야 합니다")
     private String password; // 비밀번호
+
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;  // 닉네임 고유값
 
     public User toEntity(){
